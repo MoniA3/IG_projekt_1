cf1358b9b87e20f84e9e5146294c083eafdbdc2e
majakurek <majakurek10123@gmail.com>
2024-05-03 14:51:13 +0200

    dodanie transformacji XYZ -> flh

--- proj.py
+++ proj.py
@@ -15,8 +15,8 @@ class Transformations:
         Parametry elipsoid:
             a - duża półoś elipsoidy
             b - mała półoś elipsoidy 
-            flat - spłaszczenie
-            ecc2 - mimośród^2
+            flat - biegunowe spłaszczenie elipsoidy
+            e2 - mimośród^2
         """
         if model == "WGS84":
             self.a = 6378137.0 
@@ -24,18 +24,18 @@ class Transformations:
         elif model == "GRS80":
             self.a = 6378137.0
             self.b = 6356752.31414036
-        elif model == "MARS":
-            self.a = 3396900.0
-            self.b = 3376097.80585952
+        elif model == "KRASOWSKI":
+            self.a = 6378245.0
+            self.b = 6356863.019
         else:
-            raise NotImplementedError(f"{model} model not implemented")
-        self.flat = (self.a - self.b) / self.a
-        self.ecc = sqrt(2 * self.flat - self.flat ** 2) 
-        self.ecc2 = (2 * self.flat - self.flat ** 2)
+            raise NotImplementedError(f"Program nie obsługuje tej elipsoidy")
+        self.flat = (self.a - self.b) / self.a  
+        self.e = sqrt(2 * self.flat - self.flat ** 2) 
+        self.e2 = (2 * self.flat - self.flat ** 2)
         
-        """fukcje które są niezbędne do transformacji"""
+        """Fukcje, które są niezbędne do transformacji"""
         
-        def Npu(self, fi):    
+        def Np(self, fi):    #promień krzywizny w I wertykale
             N = self.a / np.sqrt(1 - self.e2 * np.sin(fi)**2)
             return(N)
 
@@ -47,3 +47,29 @@ class Transformations:
             sigma = self.a * ( A0 * fi - A2 * np.sin(2*fi) + A4 * np.sin(4*fi) - A6 * np.sin(6*fi) )
                
             return(sigma)
+        
+        
+        """Tranformacja współrzędnych geocentrycznych XYZ na współrzędne elipsoidalne fi, lambda, h"""
+        
+        def XYZ2FLH(self, X,Y,Z):   #Algorytm Hirvonena
+            result = []
+            for X, Y, Z in zip(X,Y,Z):
+                r = np.sqrt(X**2 + Y**2) #promień
+                f = np.arctan(Z / (r * (1-self.e2)))
+                while True:
+                    N = self.Np(f)
+                    h = (r/np.cos(f)) - N
+                    fp = f
+                    f = np.arctan(Z / (r * (1-self.e2*N/(N + h))))
+                    if abs(fp-f)<(0.000001/206265):
+                        break
+                l = np.arctan2(Y,X)
+                N = self.Np(f)
+                h = (p/np.cos(f)) - N
+                result.append([np.rad2deg(f), np.rad2deg(l), h])
+            return result  
+        
+        
+        
+        
+        
