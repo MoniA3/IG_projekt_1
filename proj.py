353c26600f333139f49bc692fdee5885a9749593
majakurek <majakurek10123@gmail.com>
2024-05-11 21:47:44 +0200

    :))))

--- proj.py
+++ proj.py
@@ -26,8 +26,6 @@ class Transformacje:
         elif model == "KRASOWSKI":
             self.a = 6378245.0
             self.e2 =  0.00669342162296
-        else:
-            raise NotImplementedError(f"Program nie obsługuje podanej elipsoidy")
         
     """Fukcje, które są niezbędne do transformacji"""
         
@@ -244,20 +242,24 @@ class Transformacje:
     """wczytywanie danych oraz funckcji z pliku """
         
     def wczytywanie(self, plik, transformacja, naglowek):
-        dane = np.genfromtxt(plik, delimiter=",", skip_header=naglowek)
         if transformacja == "XYZ2flh":
+            dane = np.genfromtxt(plik, delimiter=",", skip_header=naglowek)
             flh = self.XYZ2flh(dane[:, 0], dane[:, 1], dane[:, 2])
             np.savetxt(f"Wynik_{transformacja}.txt", flh, delimiter=";")
         elif transformacja == "flh2XYZ":
+            dane = np.genfromtxt(plik, delimiter=",", skip_header=naglowek)
             xyz = self.flh2XYZ(np.deg2rad(dane[:, 0]), np.deg2rad(dane[:, 1]), dane[:, 2])
             np.savetxt(f"Wynik_{transformacja}.txt", xyz, delimiter=";")
         elif transformacja == "XYZ2NEU":
+            dane = np.genfromtxt(plik, delimiter=",", skip_header=naglowek)
             neu = self.XYZ2NEU(dane[1:, 0], dane[1:, 1], dane[1:, 2], dane[0, 0], dane[0, 1], dane[0, 2])
             np.savetxt(f"WYNIK_{transformacja}.txt", neu, delimiter=";")
         elif transformacja == "fl22000":
+            dane = np.genfromtxt(plik, delimiter=",", skip_header=naglowek)
             u2000 = self.fl22000(np.deg2rad(dane[:, 0]), np.deg2rad(dane[:, 1]))
             np.savetxt(f"WYNIK_{transformacja}.txt", u2000, delimiter=";")
         elif transformacja == "fl21992":
+            dane = np.genfromtxt(plik, delimiter=",", skip_header=naglowek)
             u1992 = self.fl21992(np.deg2rad(dane[:, 0]), np.deg2rad(dane[:, 1]))
             np.savetxt(f"WYNIK_{transformacja}.txt", u1992, delimiter=";")
                 
@@ -294,9 +296,8 @@ if __name__ == '__main__':
             print('Plik z wynikami został utworzony.')
             
             wybor = input(str("Jeżeli chcesz wykonac kolejną transformacje wpisz TAK, jesli chcesz zakonczyc wpisz KONIEC: ")).upper()
-            args.model = None
+            args.model= None
             args.dane= None
-            
             args.transformacja= None
 
     except FileNotFoundError:
